7dde3a28fb2918d577e603ccf487be3cd2c2e11a
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2024-11-14 23:30:52 +0300

    Refactor functions that select sounds for surface types.

--- Sources/EntitiesMP/Cecil/Effects.cpp
+++ Sources/EntitiesMP/Cecil/Effects.cpp
@@ -20,7 +20,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 // Surface sound for the player
 CTFileName SurfaceStepSound(CPlayer *pen) {
-  CTString strPath = "";
+  CTFileName fnmSound;
   INDEX iRnd;
 
   switch (pen->m_iLastSurface) {
@@ -40,58 +40,66 @@ CTFileName SurfaceStepSound(CPlayer *pen) {
 
   switch (pen->m_iLastSurface) {
     case SURFACE_WATER:
-      strPath.PrintF("Sounds\\Steps\\slosh%d.wav", iRnd);
+      fnmSound.PrintF("Sounds\\Steps\\slosh%d.wav", iRnd);
       break;
 
-    case SURFACE_SAND: case SURFACE_RED_SAND: case SURFACE_SNOW:
-      strPath.PrintF("Sounds\\Steps\\sand%d.wav", iRnd);
+    case SURFACE_SAND:
+    case SURFACE_RED_SAND:
+    case SURFACE_SNOW:
+      fnmSound.PrintF("Sounds\\Steps\\sand%d.wav", iRnd);
       break;
 
-    case SURFACE_GRASS: case SURFACE_GRASS_SLIDING: case SURFACE_GRASS_NOIMPACT:
-      strPath.PrintF("Sounds\\Steps\\grass%d.wav", iRnd);
+    case SURFACE_GRASS:
+    case SURFACE_GRASS_SLIDING:
+    case SURFACE_GRASS_NOIMPACT:
+      fnmSound.PrintF("Sounds\\Steps\\grass%d.wav", iRnd);
       break;
 
-    case SURFACE_WOOD: case SURFACE_WOOD_SLIDING: case SURFACE_WOOD_SLOPE:
-      strPath.PrintF("Sounds\\Steps\\wood%d.wav", iRnd);
+    case SURFACE_WOOD:
+    case SURFACE_WOOD_SLIDING:
+    case SURFACE_WOOD_SLOPE:
+      fnmSound.PrintF("Sounds\\Steps\\wood%d.wav", iRnd);
       break;
 
     case MATERIAL_CASES(METAL):
-      strPath.PrintF("Sounds\\Steps\\metal%d.wav", iRnd);
+      fnmSound.PrintF("Sounds\\Steps\\metal%d.wav", iRnd);
       break;
 
     case MATERIAL_CASES(METAL_GRATE):
-      strPath.PrintF("Sounds\\Steps\\metalgrate%d.wav", iRnd);
+      fnmSound.PrintF("Sounds\\Steps\\metalgrate%d.wav", iRnd);
       break;
 
     case MATERIAL_CASES(CHAINLINK):
-      strPath.PrintF("Sounds\\Steps\\chainlink%d.wav", iRnd);
+      fnmSound.PrintF("Sounds\\Steps\\chainlink%d.wav", iRnd);
       break;
 
     case MATERIAL_CASES(TILES):
-      strPath.PrintF("Sounds\\Steps\\tile%d.wav", iRnd);
+      fnmSound.PrintF("Sounds\\Steps\\tile%d.wav", iRnd);
       break;
 
     case MATERIAL_CASES(GLASS):
-      strPath.PrintF("Sounds\\Steps\\glass%d.wav", iRnd);
+      fnmSound.PrintF("Sounds\\Steps\\glass%d.wav", iRnd);
       break;
 
     case MATERIAL_CASES(PLASTIC):
-      strPath.PrintF("Sounds\\Impact\\plastic_soft%d.wav", iRnd);
+      fnmSound.PrintF("Sounds\\Impact\\plastic_soft%d.wav", iRnd);
       break;
 
     case MATERIAL_CASES(WEAPON):
-      strPath.PrintF("Sounds\\Steps\\weapon%d.wav", iRnd);
+      fnmSound.PrintF("Sounds\\Steps\\weapon%d.wav", iRnd);
       break;
 
-    default: strPath.PrintF("Sounds\\Steps\\concrete%d.wav", iRnd);
+    default:
+      fnmSound.PrintF("Sounds\\Steps\\concrete%d.wav", iRnd);
+      break;
   }
 
-  return CTFileName(strPath);
+  return fnmSound;
 };
 
 // Surface hit sound
 CTFileName SurfaceHitSound(CEntity *pen, INDEX iSurface) {
-  CTString strPath = "";
+  CTFileName fnmSound;
 
   switch (iSurface) {
     case SURFACE_GRASS:
@@ -100,105 +108,99 @@ CTFileName SurfaceHitSound(CEntity *pen, INDEX iSurface) {
     case SURFACE_SAND:
     case SURFACE_RED_SAND:
     case SURFACE_SNOW:
-      strPath.PrintF("Sounds\\Impact\\sand%d.wav", pen->IRnd()%4 + 1);
+      fnmSound.PrintF("Sounds\\Impact\\sand%d.wav", pen->IRnd() % 4 + 1);
       break;
 
     case SURFACE_WATER:
-      strPath.PrintF("Sounds\\Steps\\slosh%d.wav", pen->IRnd()%4 + 1);
+      fnmSound.PrintF("Sounds\\Steps\\slosh%d.wav", pen->IRnd() % 4 + 1);
       break;
 
-    case SURFACE_WOOD: case SURFACE_WOOD_SLIDING: case SURFACE_WOOD_SLOPE:
-      strPath.PrintF("Sounds\\Impact\\wood%d.wav", pen->IRnd()%5 + 1);
+    case SURFACE_WOOD:
+    case SURFACE_WOOD_SLIDING:
+    case SURFACE_WOOD_SLOPE:
+      fnmSound.PrintF("Sounds\\Impact\\wood%d.wav", pen->IRnd() % 5 + 1);
       break;
 
     case MATERIAL_CASES(METAL):
-      strPath.PrintF("Sounds\\Impact\\metal%d.wav", pen->IRnd()%4 + 1);
+      fnmSound.PrintF("Sounds\\Impact\\metal%d.wav", pen->IRnd() % 4 + 1);
       // [Cecil] TODO: Use these sounds specifically on the crowbar weapon item instead
-      //strPath.PrintF("Models\\Weapons\\Crowbar\\Sounds\\Impact%d.wav", pen->IRnd()%2 + 1);
+      //fnmSound.PrintF("Models\\Weapons\\Crowbar\\Sounds\\Impact%d.wav", pen->IRnd() % 2 + 1);
       break;
 
     case MATERIAL_CASES(METAL_GRATE):
-      strPath.PrintF("Sounds\\Impact\\metal%d.wav", pen->IRnd()%4 + 1);
+      fnmSound.PrintF("Sounds\\Impact\\metal%d.wav", pen->IRnd() % 4 + 1);
       break;
 
     case MATERIAL_CASES(CHAINLINK):
-      strPath.PrintF("Sounds\\Steps\\chainlink%d.wav", pen->IRnd()%4 + 1);
+      fnmSound.PrintF("Sounds\\Steps\\chainlink%d.wav", pen->IRnd() % 4 + 1);
       break;
 
     case MATERIAL_CASES(TILES):
-      strPath.PrintF("Sounds\\Impact\\tile%d.wav", pen->IRnd()%4 + 1);
+      fnmSound.PrintF("Sounds\\Impact\\tile%d.wav", pen->IRnd() % 4 + 1);
       break;
 
     case MATERIAL_CASES(GLASS):
-      strPath.PrintF("Sounds\\Impact\\glass%d.wav", pen->IRnd()%4 + 1);
+      fnmSound.PrintF("Sounds\\Impact\\glass%d.wav", pen->IRnd() % 4 + 1);
       break;
 
     case MATERIAL_CASES(PLASTIC):
-      strPath.PrintF("Sounds\\Impact\\plastic_hard%d.wav", pen->IRnd()%4 + 1);
+      fnmSound.PrintF("Sounds\\Impact\\plastic_hard%d.wav", pen->IRnd() % 4 + 1);
       break;
 
     case MATERIAL_CASES(WEAPON):
-      strPath.PrintF("Sounds\\Impact\\weapon_bullet%d.wav", pen->IRnd()%3 + 1);
+      fnmSound.PrintF("Sounds\\Impact\\weapon_bullet%d.wav", pen->IRnd() % 3 + 1);
       break;
 
-    default: strPath.PrintF("Sounds\\Impact\\concrete%d.wav", pen->IRnd()%4 + 1);
+    default:
+      fnmSound.PrintF("Sounds\\Impact\\concrete%d.wav", pen->IRnd() % 4 + 1);
+      break;
   }
-  
-  return CTFileName(strPath);
+
+  return fnmSound;
 };
 
 // Particles sound
 CTFileName SprayParticlesSound(CEntity *pen, SprayParticlesType spt) {
+  CTFileName fnmSound;
+
   switch (spt) {
     case SPT_BLOOD:
     case SPT_FEATHER:
     case SPT_SLIME:
-    case SPT_GOO: {
-      CTString strSound = "";
-      strSound.PrintF("Sounds\\Impact\\flesh%d.wav", pen->IRnd()%5 + 1);
-      return CTFileName(strSound);
-    }
+    case SPT_GOO:
+      fnmSound.PrintF("Sounds\\Impact\\flesh%d.wav", pen->IRnd() % 5 + 1);
+      break;
 
     case SPT_BONES:
     case SPT_WOOD:
-    case SPT_TREE01: {
-      CTString strSound = "";
-      strSound.PrintF("Sounds\\Impact\\wood%d.wav", pen->IRnd()%5 + 1);
-      return CTFileName(strSound);
-    }
+    case SPT_TREE01:
+      fnmSound.PrintF("Sounds\\Impact\\wood%d.wav", pen->IRnd() % 5 + 1);
+      break;
 
     case SPT_STONES:
     case SPT_LAVA_STONES:
     case SPT_SMALL_LAVA_STONES:
     case SPT_BEAST_PROJECTILE_SPRAY:
-    case SPT_COLOREDSTONE: {
-      CTString strSound = "";
-      strSound.PrintF("Sounds\\Impact\\concrete%d.wav", pen->IRnd()%4 + 1);
-      return CTFileName(strSound);
-    }
+    case SPT_COLOREDSTONE:
+      fnmSound.PrintF("Sounds\\Impact\\concrete%d.wav", pen->IRnd() % 4 + 1);
+      break;
 
     case SPT_ELECTRICITY_SPARKS:
-    case SPT_ELECTRICITY_SPARKS_NO_BLOOD: {
-      CTString strSound = "";
-      strSound.PrintF("Sounds\\Impact\\metal%d.wav", pen->IRnd()%4 + 1);
-      return CTFileName(strSound);
-    }
+    case SPT_ELECTRICITY_SPARKS_NO_BLOOD:
+      fnmSound.PrintF("Sounds\\Impact\\metal%d.wav", pen->IRnd() % 4 + 1);
+      break;
 
     case SPT_AIRSPOUTS:
-    case SPT_PLASMA: {
-      CTString strSound = "";
-      strSound.PrintF("Sounds\\Impact\\plastic_hard%d.wav", pen->IRnd()%4 + 1);
-      return CTFileName(strSound);
-    }
+    case SPT_PLASMA:
+      fnmSound.PrintF("Sounds\\Impact\\plastic_hard%d.wav", pen->IRnd() % 4 + 1);
+      break;
 
-    default: {
-      CTString strSound = "";
-      strSound.PrintF("Models\\Weapons\\Crowbar\\Sounds\\Impact%d.wav", pen->IRnd()%2 + 1);
-      return CTFileName(strSound);
-    }
+    default:
+      fnmSound.PrintF("Models\\Weapons\\Crowbar\\Sounds\\Impact%d.wav", pen->IRnd() % 2 + 1);
+      break;
   }
-  
-  return CTString("");
+
+  return fnmSound;
 };
 
 // Get placement of an attachment
